abe5baadef7d97e08e0a785cf32dc1736764b8a3
Pedro Sena <105237982+PedroSena1@users.noreply.github.com>
2022-08-13 10:30:12 -0300

    Add files via upload

---
+++ Programa  Navegador.py
@@ -0,0 +1,21 @@
+#Você foi contratado para desenvolver um programa similar ao funcionamento do Waze 
+#(navegador de bordo). 
+#Ele terá que calcular a distância percorrida pelo automóvel considerando 
+#as direções (siga em frente, vire a esquerda, vire a direita ou pare). 
+
+#O programa deve receber um número indefinido de direções que o carro está seguindo, 
+# e a cada uma delas atribui-se o valor percorrido de 100.
+
+#uando for digitado "parar", o programa deve exibir distância percorrida em Km.
+
+#Exemplo:
+
+#Entrada:
+#siga em frente 
+#vire a esquerda 
+#vire a direita
+#siga em frente
+#parar
+#Saída
+#Distância percorrida = 0,4 Km
+#_--------------------------------------------------------------------------------------------
